04364e9500332d8f2959e3ddf2616f270f583599
RachelEversmann <Rachel.Eversmann@gamail.com>
2017-10-04 17:15:52 -0500

    add and deleted is fully working" ;

--- client/src/List.js
+++ client/src/List.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react'
+import React from 'react'
 import {TableRow, TableRowColumn, FlatButton} from 'material-ui';
 import RaisedButton from 'material-ui/RaisedButton';
 import Dialog from 'material-ui/Dialog';
